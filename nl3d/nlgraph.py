1baa173eaf85a4b3282c7011f27ddc71832fdedb
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2017-05-24 00:30:05 +0900

    nl3d/nlgraph.py を追加.

---
+++ nl3d/nlgraph.py
@@ -0,0 +1,223 @@
+#! /usr/bin/env python3
+
+## @file nlgraph.py
+# @brief NlGraph の定義ファイル
+# @author Yusuke Matsunaga (松永 裕介)
+#
+# Copyright (C) 2017 Yusuke Matsunaga
+# All rights reserved.
+
+from nlpoint import NlPoint
+from nlvia import NlVia
+from nlproblem import NlProblem
+
+
+## @brief 節点を表すクラス
+#
+# 以下のメンバを持つ．
+# - 座標(_x, _y, _z)
+# - 接続している枝のリスト(_edge_list)
+# - 線分番号を表す変数のリスト(_var_list)
+# - 終端の時に True となるフラグ(_is_terminal)
+# - 終端の時の線分番号(_terminal_id)
+# - ビアの時に True となるフラグ(_is_via)
+# - ビアの時のビア番号(_via_id)
+class NlNode :
+
+    ## @brief 初期化
+    def __init__(self, x, y, z) :
+        self._x = x
+        self._y = x
+        self._z = z
+        self._var = None
+        self._edge_list = []
+        self._var_list = []
+        self._is_terminal = False
+        self._terminal_id = None
+        self._is_via = False
+        self._via_id = None
+
+    ## @brief 終端の印をつける．
+    def set_terminal(self, net_id) :
+        self._is_terminal = True
+        self._terminal_id = net_id
+
+    ## @brief ビアの印をつける．
+    def set_via(self, via_id) :
+        self._is_via = True
+        self._via_id = via_id
+
+    ## @brief 枝を追加する．
+    def add_edge(self, edge) :
+        self._edge_list.append(edge)
+
+    ## @brief X座標
+    @property
+    def x(self) :
+        return self._x
+
+    ## @brief Y座標
+    @property
+    def y(self) :
+        return self._y
+
+    ## @brief Z座標
+    @property
+    def z(self) :
+        return self._z
+
+    ## @brief 接続している枝のリストを返す
+    @property
+    def edge_list(self) :
+        return self._edge_list
+
+    ## @brief 終端フラグ
+    @property
+    def is_terminal(self) :
+        return self._is_terminal
+
+    ## @brief 終端番号
+    #
+    # is_terminal == False の場合の値は不定
+    @property
+    def terminal_id(self) :
+        return self._terminal_id
+
+    ## @brief ビアフラグ
+    @property
+    def is_via(self) :
+        return self._is_via
+
+    ## @brief ビア番号
+    #
+    # is_via == False の場合の値は不定
+    @property
+    def via_id(self) :
+        return self._via_id
+
+
+## @brief 枝を表すクラス
+#
+# 以下のメンバを持つ．
+# - 両端の節点(_node1, _node2)と
+# - 自身が線分として選ばれている時 True
+#   となる命題変数の番号(_var)を持つ．
+class NlEdge :
+
+    ## @brief 初期化
+    def __init__(self, node1, node2, var) :
+        self._node1 = node1
+        self._node2 = node2
+        self._var = var
+
+    ## @brief ノード1
+    @property
+    def node1(self) :
+        return self._node1
+
+    ## @brief ノード2
+    @property
+    def node2(self) :
+        return self._node2
+
+
+## @brief ナンバーリンクの問題を表すグラフ
+class NlGraph :
+
+    ## @brief 初期化
+    #
+    # @param[in] problem 問題を表すオブジェクト(NlProblem)
+    def __init__(self, problem) :
+        w = problem.width
+        h = problem.height
+        d = problem.depth
+        nn = problem.net_num
+        vn = problem.via_num
+
+        self._net_num = nn
+        self._via_num = vn
+
+        # ネット数の log を求める．
+        nn_log = 0
+        while (1 << nn_log) < nn :
+            nn_log += 1
+
+        # 節点を作る．
+        # node_array[x][y][z] に (x, y, z) の節点が入る．
+        # Python 特有の内包表記で one-liner で書けるけど1行が長すぎ．
+        node_array = [[[self._new_node(x, y, z) for z in range(0, d)] \
+                       for y in range(0, h)] for x in range(0, w)]
+
+        # 枝を作る．
+        for z in range(0, d) :
+            # 水平の枝を作る．
+            for x in range(0, w - 1) :
+                for y in range(0, h) :
+                    # (x, y) - (x + 1, y) を結ぶ枝
+                    node1 = node_array[x][y][z]
+                    node2 = node_array[x + 1][y][z]
+                    self._new_edge(node1, node2)
+
+            # 垂直の枝を作る．
+            for x in range(0, w) :
+                for y in range(0, h - 1) :
+                    # (x, y) - (x, y + 1) を結ぶ枝
+                    node1 = node_array[x][y][z]
+                    node2 = node_array[x][y + 1][z]
+                    self._new_edge(node1, node2)
+
+        # 端子の印をつける．
+        for net_id, (label, s, e) in problem.net_list() :
+            node1 = node_array[s.x][s.y][s.z]
+            node2 = node_array[e.x][e.y][e.z]
+            node1.set_terminal(net_id)
+            node2.set_terminal(net_id)
+
+        # ビアの印をつける．
+        for via_id, via in problem.via_list() :
+            for z in range(via.z1, via.z2 - via.z1 + 1) :
+                node = node_array[via.x][via.y][z]
+                node.set_via(via_id)
+
+    ## @brief ネット数
+    @property
+    def net_num(self) :
+        return self._net_num
+
+
+    ## @brief ビア数
+    @property
+    def via_num(self) :
+        return self._via_num
+
+
+    ## @brief ノードのリストを返す．
+    @property
+    def node_list(self) :
+        return self._node_list
+
+
+    ## @brief 枝のリストを返す．
+    @property
+    def edge_list(self) :
+        return self._edge_list
+
+
+    ## @brief 枝を作る．
+    # @param[in] node1, node2 両端の節点
+    def _new_edge(self, node1, node2) :
+        edge = NlEdge(node1, node2)
+        self._edge_list.append(edge)
+        node1.add_edge(edge)
+        node2.add_edge(edge)
+
+
+    ## @brief ノードを作る．
+    # @param[in] x, y, z 座標
+    #
+    # 結果を self._node_list に入れる．
+    def _new_node(self, x, y, z) :
+        node = NlNode(x, y, z)
+        self._node_list.append(node)
+
+        return node
--- nl3d/nlsolver.py
+++ nl3d/nlsolver.py
@@ -7,146 +7,154 @@
 # Copyright (C) 2017 Yusuke Matsunaga
 # All rights reserved.
 
-from nlpoint import NlPoint
-from nlvia import NlVia
-from nlproblem import NlProblem
+from nlgraph import NlNode, NlEdge, NlGraph
+from nl3d.sat.satsolver import SatSolver
+from nl3d.sat.satbool3 import Sat
+
+## @brief 問題を表すCNF式を生成する．
+# @param[in] graph 問題を表すグラフ(NlGraph)
+# @param[in] solver SATソルバ
+def solve_nlink(graph, solver) :
+    nn = graph.net_num
+    vn = graph.via_num
+
+    # 枝に対応する変数を作る．
+    # 結果は edge_var_dict に edge をキーとして格納する．
+    edge_var_dict = [edge:solver.new_variable() for edge in graph.edge_list]
+
+    # 節点のラベルを表す変数のリストを作る．
+    # 結果は node_vars_dict に node をキーとして格納する．
+    node_vars_dict = [node:[solver.new_variable() for in in range(0, nn)] \
+                      for node in graph.node_list]
+
+    # ビアと線分の割り当てを表す変数を作る．
+    # nv_map[net_id][via_id] に net_id の線分を via_id のビアに接続する時 True となる変数を入れる．
+    nv_map = [[solver.new_variable() for via_id in range(0, nv)] for net_id in range(0, nn)]
+
+    # 各節点に対して隣接する枝の条件を作る．
+    # 具体的には
+    # - 終端の場合
+    #   ただ一つの枝のみが選ばれる．
+    # - ビアの場合
+    #   nv_map の変数
+    # - それ以外
+    #   全て選ばれないか2つの枝が選ばれる．
+    for node in graph.node_list :
+        # node に接続している枝の変数のリスト
+        evar_list = [edge_var_dict[edge] for edge in node.edge_list]
+
+        # node のラベルを表す変数のリスト
+        lvar_list = node_vars_dict[node]
+
+        if node.is_terminal :
+            # node が終端の場合
+            # ラベルの変数を固定する．
+            tid = node.terminal_id
+            for i in range(0, nn) :
+                lvar = lvar_list[i]
+                if i == tid :
+                    solver.add_clause(lvar)
+                else :
+                    solver.add_clause(-lvar)
+            # ただ一つの枝が選ばれる．
+            _make_one(solver, evar_list)
+        elif node.is_via :
+            # node がビアの場合
+            # この層に終端を持つ線分と結びついている時はただ一つの枝が選ばれる．
+            vid = node.via_id
+            for net_id in range(0, nn) :
+                # そうでなければ全部 False
+        else :
+            # それ以外の場合は０か２個の枝が選ばれる．
+            _make_zero_or_two(solver, evar_list)
+
+    # 枝が選択された時にその両端のノードのラベルが等しくなるという制約を作る．
+    for edge in graph.edge_list :
+        evar = edge_var_dict[edge]
+        nvar_list1 = node_vars_dict[edge.node1]
+        nvar_list2 = node_vars_dict[edge.node2]
+        for i in range(0, nn) :
+            nvar1 = nvar_list1[i]
+            nvar2 = nvar_list2[i]
+            solver.add_clause(-evar, -nvar1,  nvar2)
+            solver.add_clause(-evar,  nvar1, -nvar2)
+
+    # SAT問題を解く．
+    result, model = solver.solve()
+
+    if result == SatBool3.B3True :
+        # 解けた．
+        print("OK")
+    elif result == SatBool3.B3False :
+        # 解けなかった．
+        print("NG")
+    elif result == SatBool3.B3X :
+        # アボートした．
+        print("Abort")
 
 
-## @brief 節点を表すクラス
-class NlNode :
-
-    ## @brief 初期化
-    def __init__(self, x, y, z) :
-        self._x = x
-        self._y = x
-        self._z = z
-        self._var = None
-        self._edge_list = []
-        self._var_list = []
-        self._is_terminal = False
-        self._is_via = False
-        self._via_id = None
-
-
-## @brief 枝を表すクラス
-class NlEdge :
-
-    ## @brief 初期化
-    def __init__(self, node1, node2, var) :
-        self._node1 = node1
-        self._node2 = node2
-        self._var = var
-
-
-## @brief ナンバーリンクソルバーを表すクラス
-class NlSolver :
-
-    ## @brief 初期化
-    def __init__(self) :
-        pass
-
-    ## @brief 問題を表すCNF式を生成する．
-    # @param[in] problem 問題を表すオブジェクト(NlProblem)
-    # @param[in] solver SATソルバ
-    def make_cnf(self, problem, solver) :
-        w = problem.width
-        h = problem.height
-        d = problem.depth
-        nn = problem.net_num
-        vn = problem.via_num
-
-        # ネット数の log を求める．
-        nn_log = 0
-        while (1 << nn_log) < nn :
-            nn_log += 1
-
-        # 節点を作る．
-        # node_array[x][y][z] に (x, y, z) の節点が入る．
-        node_array = [[[NlNode(x, y, z) for z in range(0, d)] for y in range(0, h)] for x in range(0, w)]
-
-        # 枝を作る．
-        for z in range(0, d) :
-            # 水平の枝に番号を振る．
-            for x in range(0, w - 1) :
-                for y in range(0, h) :
-                    # (x, y) - (x + 1, y) を結ぶ枝
-                    node1 = node_array[x][y][z]
-                    node2 = node_array[x + 1][y][z]
-                    self.new_edge(node1, node2, solver)
-
-            # 垂直の枝に番号を振る．
-            for x in range(0, w) :
-                for y in range(0, h - 1) :
-                    # (x, y) - (x, y + 1) を結ぶ枝
-                    node1 = node_array[x][y][z]
-                    node2 = node_array[x][y + 1][z]
-                    self.new_edge(node1, node2, solver)
-
-        # 節点のラベルを表す変数を作る．
-        for z in range(0, d) :
-            for x in range(0, w) :
-                for y in range(0, h) :
-                    node = node_array[x][y][z]
-                    node._var_list = [solver.new_variable() for i in range(0, nn_log)]
-
-        # ビアと線分の割り当てを表す変数を作る．
-        # nv_map[net_id][via_id] に net_id の線分を via_id のビアに接続する時 True となる変数を入れる．
-        nv_map = [[solver.new_variable() for via_id in range(0, nv)] for net_id in range(0, nn)]
-
-        # 端子の印をつける．
-        for net_id, (label, s, e) in problem.net_list() :
-            node1 = node_array[s.x][s.y][s.z]
-            node2 = node_array[e.x][e.y][e.z]
-            self.set_terminal(node1, net_id, solver)
-            self.set_terminal(node2, net_id, solver)
-
-        # ビアの印をつける．
-        for via_id, via in problem.via_list() :
-            for z in range(via.z1, via.z2 - via.z1 + 1) :
-                node = node_array[via.x][via.y][z]
-                self.set_via(node, via_id, solver)
-
-        # 各節点に対して隣接する枝の条件を作る．
-        # 具体的には
-        # - 終端の場合
-        #   ただ一つの枝のみが選ばれる．
-        # - ビアの場合
-        #   nv_map の変数
-        # - それ以外
-        #   全て選ばれないか2つの枝が選ばれる．
-        for z in range(0, d) :
-            for x in range(0, w) :
-                for y in range(0, h) :
-                    node = node_array[x][y][z]
-                    var_list = [edge._var for edge in node._edge_list]
-                    if node._is_terminal :
-                        self.make_one(var_list, solver)
-                    elif node._is_via :
-                    else :
-                        self.make_zero_or_two(var_list, solver)
+## @brief リストの中の変数が1つだけ True となる制約を作る．
+def _make_one(solver, var_list) :
+    n = len(var_list)
+    # 要素数で場合分け
+    if n == 2 :
+        var0 = var_list[0]
+        var1 = var_list[1]
+        solver.add_clause(-var0, -var1)
+        solver.add_clause( var0,  var1)
+    elif n == 3 :
+        var0 = var_list[0]
+        var1 = var_list[1]
+        var2 = var_list[2]
+        solver.add_clause(-var0, -var1       )
+        solver.add_clause(-var0,      , -var2)
+        solver.add_clause(       -var1, -var2)
+        solver.add_clause( var0,  var1,  var2)
+    elif n == 4 :
+        var0 = var_list[0]
+        var1 = var_list[1]
+        var2 = var_list[2]
+        var3 = var_list[3]
+        solver.add_clause(-var0, -var1              )
+        solver.add_clause(-var0,        -var2       )
+        solver.add_clause(-var0,               -var3)
+        solver.add_clause(       -var1, -var2       )
+        solver.add_clause(       -var1,        -var3)
+        solver.add_clause(              -var2, -var3)
+        solver.add_clause( var0,  var1,  var2,  var3)
+    else :
+        assert False
 
 
-    ## @brief 枝を作る．
-    # @param[in] node1, node2 両端の節点
-    # @param[in] solver SATソルバ
-    def new_edge(self, node1, node2, solver) :
-        var = solver.new_variable()
-        edge = NlEdge(node1, node2, var)
-        node1._edge_list.append(edge)
-        node2._edge_list.append(edge)
-
-
-    ## @brief 節点に終端の印をつける．
-    def set_terminal(self, node, net_id, solver) :
-        node._is_terminal = True
-        # ラベルを固定する．
-        for i, var in enumerate(node._var_list) :
-            if (1 << i) & net_id :
-                solver.add_clause(var)
-            else :
-                solver.add_clause(-var)
-
-    ## @brief 節点にビアの印をつける．
-    def set_via(self, node, via_id, solver) :
-        node._is_via = True
-        node._via_id = via_id
+## @brief リストの中の変数が0個か2個 True になるという制約
+def _make_zero_or_two(solver, var_list) :
+    n = len(var_list)
+    # 要素数で場合分け
+    if n == 2 :
+        var0 = var_list[0]
+        var1 = var_list[1]
+        solver.add_clause( var0, -var1)
+        solver.add_clause(-var0,  var1)
+    elif n == 3 :
+        var0 = var_list[0]
+        var1 = var_list[1]
+        var2 = var_list[2]
+        solver.add_clause(-var0, -var1, -var2)
+        solver.add_clause( var0,  var1, -var2)
+        solver.add_clause( var0, -var1,  var2)
+        solver.add_clause(-var0,  var1,  var2)
+    elif n == 4 :
+        var0 = var_list[0]
+        var1 = var_list[1]
+        var2 = var_list[2]
+        var3 = var_list[3]
+        solver.add_clause(-var0, -var1, -var2       )
+        solver.add_clause(-var0, -var1,      , -var3)
+        solver.add_clause(-var0,        -var2, -var3)
+        solver.add_clause(       -var1, -var2, -var3)
+        solver.add_clause( var0,  var1,  var2, -var3)
+        solver.add_clause( var0,  var1, -var2,  var3)
+        solver.add_clause( var0, -var1,  var2,  var3)
+        solver.add_clause(-var0,  var1,  var2,  var3)
+    else :
+        assert False
